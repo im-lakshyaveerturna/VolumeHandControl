490bfb5291f38d21016fcea2901540412d24a134
Lakshyaveer Turna <th.is.lakshyaveerturna@gmail.com>
2024-12-04 16:56:25 +0530

    commit

--- practice.py
+++ practice.py
@@ -1,24 +1,20 @@
 import cv2
 import mediapipe as mp
 
-
-
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 mpHands = mp.solutions.hands
 mpDraw = mp.solutions.drawing_utils
-hands = mpHands.Hands()
-
+Hands = mpHands.Hands()
 
 while True:
     success, image = cap.read()
-    imgRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
+    RBGimg = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    results = Hands.process(RBGimg)
 
     if results.multi_hand_landmarks:
-
         for handLms in results.multi_hand_landmarks:
             mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
 
-    cv2.imshow('Video',image)
+    cv2.imshow("Video", image)
     cv2.waitKey(1)
