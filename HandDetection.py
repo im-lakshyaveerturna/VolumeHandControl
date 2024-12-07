515d5be089c0bd046db9e40c985d909309ad0325
Lakshyaveer Turna <th.is.lakshyaveerturna@gmail.com>
2024-12-07 17:52:58 +0530

    commit

--- HandDetection.py
+++ HandDetection.py
@@ -1,15 +1,17 @@
 import cv2
 import mediapipe as mp
+import time
 
 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 mpHands = mp.solutions.hands
 mpDraw = mp.solutions.drawing_utils
-hands = mpHands.Hands()
+hands = mpHands.Hands(False,2,1,0.8,0.8)
 
-
+pTime = 0
+cTime = 0
 while True:
     success, image = cap.read()
     imgRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -18,7 +20,15 @@ while True:
     if results.multi_hand_landmarks:
 
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                print(id, lm.x, lm.y)
+
             mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
 
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(image, "FPS"+str(int(fps)), (10,70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     cv2.imshow('Video',image)
     cv2.waitKey(1)
